548cea04efc2e593b0a3334c1e0769dbdcd94c3d
ForseIKomar <33393253+ForseIKomar@users.noreply.github.com>
2018-10-15 10:32:14 +0300

    1
    1

---
+++ Теория информации/Теория информации дз/program.pas
@@ -0,0 +1,37 @@
+﻿var
+  f: text;
+  i: integer;
+  sum: integer;
+  S: string;
+  H, p: real;
+  chars: array [1..10000] of integer;
+begin
+  assign(f, 'my.txt');
+  reset(f);
+  while not EOF(f) do begin
+    readln(f, S);
+    for i := 1 to length(S) do begin
+      if (S[i] >= 'А') and (S[i] <= 'Я') then
+        S[i] := char(word(S[i]) - word('А') + word('а'));
+      chars[ord(S[i])] += 1;
+    end;
+    sum += length(S);
+  end;
+  
+  for i := word('а') to word('я') do begin
+    p := chars[i] / sum;
+    writeln(i: 5, ': ', chr(i), ': ', chars[i], '  p = ', p: 5: 4);
+    if (chars[i] > 0) then
+      H -= (chars[i] / sum) * (ln(chars[i] / sum) / ln(33));
+  end;
+  
+  writeln('H = ', H);
+  
+  close(f);
+end.
+// H = M(-log(pi))
+// 1 - Частоты букв в тексте, средняя энтропия буквы
+// 2 - Частоты появления слов, средняя энтропия слова
+// Повторить график Леона Бриллюэна
+// Сжать, применив кодирование К.Шеннона
+// Найти или написать программу для исследования, выполненого Марковым А.А.
